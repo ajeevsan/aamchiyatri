7f489d32dd7086ba940393bf5b6e0a13b2774cb4
sanjeev.majhi.con <sanjeev.majhi.con@hubblehox.com>
2026-08-12 17:15:29 +0530

    adding all the working services

--- functions/index.js
+++ functions/index.js
@@ -3,8 +3,5 @@ const { initializeApp } = require("firebase-admin/app");
 initializeApp();
 
 const { onRideCreated } = require("./dispatch");
-const { createRazorpayOrder, verifyRazorpayPayment } = require("./payments");
 
 exports.onRideCreated = onRideCreated;
-exports.createRazorpayOrder = createRazorpayOrder;
-exports.verifyRazorpayPayment = verifyRazorpayPayment;
